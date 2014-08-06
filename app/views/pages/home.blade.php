80d4c74c0fc682970037563675e4decc49911bfd
Drew Schmaltz <drew@mainstreetmower.com>
2014-08-05 19:50:12 -0500

    register jumbotron

--- app/views/pages/home.blade.php
+++ app/views/pages/home.blade.php
@@ -1,18 +1,15 @@
 @extends('layouts.default')
 
 @section('content')
-  <div class="row">
-    <div class="col-lg-12">
-      <div class="jumbotron">
-        <h1>Buy and Sell Lawn Mowers</h1>
-        <p>Welcome to MowerMatcher, the place to buy and sell your lawn mowers for free.  Anyone can search and members can list their mowers.</p>
-        <p>Our Smart Search allows you to save searches and get notified when other users post mowers that fit your needs.</p>
-        <p>
-          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">Register »</a>
-        </p>
+
+  @if (! $currentUser)
+    <div class="row">
+      <div class="col-lg-12">
+        @include ('layouts.partials.register-jumbotron')
       </div>
     </div>
-  </div>
+  @endif
+
   <div class="row" ng-controller="filterMowers">
     <div class="col-md-3">
       @include ('mowers.partials.search-mower-form')
