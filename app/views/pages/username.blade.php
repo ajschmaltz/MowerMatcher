8fd1bfaa165cccbf547e78c3234ab4373ee68495
Andrew Schmaltz <Drew@Andrews-MacBook-Pro.local>
2014-08-29 08:16:16 -0400

    changes

--- app/views/pages/username.blade.php
+++ app/views/pages/username.blade.php
@@ -3,12 +3,10 @@
 @section('content')
 <div class="row">
   <div class="col-md-3">
-    @include ('users.sidenav')
+    @include ('users.profile')
   </div>
   <div class="col-md-9">
-
     @include ('mowers.mower-gallery', ['mowers' => $user->mowers])
-
   </div>
 </div>
 @stop
